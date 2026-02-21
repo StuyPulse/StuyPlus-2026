064503903b14548e8fbd809245f41baad2969e1e
hc-45 <223769218+hc-45@users.noreply.github.com>
2026-02-21 09:59:34 -0500

    FIX: fix swerve using alphabot gains

--- src/main/java/com/stuypulse/robot/constants/Gains.java
+++ src/main/java/com/stuypulse/robot/constants/Gains.java
@@ -21,39 +21,34 @@ public class Gains {
     }
 
     public interface Swerve {
-        double kP = 0;
-        double kI = 0;
-        double kD = 0;
+        public interface Drive {
+            double kS = 0.0;
+            double kV = 0.124;
+            double kA = 0.0;
+            double kP = 0.1;
+            double kI = 0.0;
+            double kD = 0.0;
+        }
 
-        double kS = 0;
-        double kV = 0;
-        double kA = 0;
-        double kG = 0;
+        public interface Turn {
+            double kS = 0.1;
+            double kV = 2.66;
+            double kA = 0.0;
+            double kP = 100.0;
+            double kI = 0.0;
+            double kD = 0.5;
+        }
 
         public interface Alignment {
-            PIDConstants XY = new PIDConstants(0, 0, 0);
-            PIDConstants THETA = new PIDConstants(0, 0, 0);
-        }
+            double kP = 0.0;
+            double kI = 0.0;
+            double kD = 0.0;
+            double akP = 0.0;
+            double akI = 0.0;
+            double akD = 0.0;
 
-        public interface Drive {
-            double kP = 0;
-            double kI = 0;
-            double kD = 0;
-
-            double kS = 0;
-            double kV = 0;
-            double kA = 0;
-            double kG = 0;
-        }
-        public interface Turn {
-            double kP = 0;
-            double kI = 0;
-            double kD = 0;
-
-            double kS = 0;
-            double kV = 0;
-            double kA = 0;
-            double kG = 0;
+            PIDConstants XY = new PIDConstants(0.0, 0.0, 0.0);
+            PIDConstants THETA = new PIDConstants(0.0, 0.0, 0.0);
         }
     }
 }
