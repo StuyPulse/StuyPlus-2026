2f41fb29d2fcead8a5914bbd259fed6c3c2d3405
aidenzhang21345 <aidenzhang211@gmail.com>
2026-04-06 22:29:38 -0400

    FEAT: Added pipeline setting for limelight

---
+++ src/main/java/com/stuypulse/robot/commands/vision/SetPipeline.java
@@ -0,0 +1,17 @@
+package com.stuypulse.robot.commands.vision;
+
+import com.stuypulse.robot.util.vision.LimelightHelpers;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+public class SetPipeline extends InstantCommand{
+    private int pipeline;
+    public SetPipeline(int pipeline) {
+        this.pipeline = pipeline;
+    }
+
+    @Override
+    public void initialize() {
+        LimelightHelpers.setPipelineIndex("limelight", pipeline);
+    }
+}
