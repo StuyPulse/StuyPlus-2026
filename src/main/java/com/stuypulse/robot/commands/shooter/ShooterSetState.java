a54803c6c88823a6eca110fda13a01213535cdfd
Faizaan J <125227430+Faizaan-J@users.noreply.github.com>
2026-04-15 18:30:55 -0400

    FIX: Set shooter field to shooter instance
    Fix incorrect this.shooter = shooter; to this.shooter = Shooter.getInstance();

--- src/main/java/com/stuypulse/robot/commands/shooter/ShooterSetState.java
+++ src/main/java/com/stuypulse/robot/commands/shooter/ShooterSetState.java
@@ -5,15 +5,12 @@ import com.stuypulse.robot.subsystems.shooter.Shooter;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
-//TODO: Make sure Faizaan pushes to fix this, before that it gon give error
-
 public class ShooterSetState extends InstantCommand {
     private Shooter shooter;
     private ShooterState shooterState; 
     
-    
     public ShooterSetState(ShooterState shooterState){
-        this.shooter = shooter;
+        this.shooter = Shooter.getInstance();
         this.shooterState = shooterState;
 
         addRequirements(shooter);
