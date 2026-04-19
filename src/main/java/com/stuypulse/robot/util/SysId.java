24bdf3cdee6f3881d25676666e777b2ba03d2884
hc-45 <223769218+hc-45@users.noreply.github.com>
2026-04-19 15:30:40 -0400

    FEAT: method overload that accepts WPILib units

--- src/main/java/com/stuypulse/robot/util/SysId.java
+++ src/main/java/com/stuypulse/robot/util/SysId.java
@@ -5,7 +5,12 @@ import java.util.function.Supplier;
 
 import com.ctre.phoenix6.SignalLogger;
 
+import edu.wpi.first.units.measure.*;
+import static edu.wpi.first.units.Units.*;
+
 import edu.wpi.first.units.Units;
+import edu.wpi.first.units.VoltageUnit;
+
 import static edu.wpi.first.units.Units.Second;
 import static edu.wpi.first.units.Units.Volts;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -35,6 +40,32 @@ public class SysId {
                             SignalLogger.writeDouble(subsystemName + " Position", positionSupplier.get());
                             SignalLogger.writeDouble(subsystemName + " Velocity", velocitySupplier.get());
                             SignalLogger.writeDouble(subsystemName + " Voltage", voltageSupplier.get());
+                        },
+                        subsystemInstance));
+    }
+
+    public static SysIdRoutine getRoutine(
+            Velocity<VoltageUnit> rampRate,
+            Voltage stepVoltage,
+            String subsystemName,
+            Consumer<Voltage> voltageSetter,
+            Supplier<Angle> positionSupplier,
+            Supplier<AngularVelocity> velocitySupplier,
+            Supplier<Voltage> voltageSupplier,
+            Subsystem subsystemInstance
+    ) {
+        return new SysIdRoutine(
+                new SysIdRoutine.Config(
+                        rampRate,
+                        stepVoltage,
+                        null,
+                        state -> SignalLogger.writeString(subsystemName + " SysId-State", state.toString())),
+                new SysIdRoutine.Mechanism(
+                        output -> voltageSetter.accept(output),
+                        state -> {
+                            SignalLogger.writeDouble(subsystemName + " Position", positionSupplier.get().in(Rotations));
+                            SignalLogger.writeDouble(subsystemName + " Velocity", velocitySupplier.get().in(RotationsPerSecond));
+                            SignalLogger.writeDouble(subsystemName + " Voltage", voltageSupplier.get().in(Volts));
                         },
                         subsystemInstance));
     }
