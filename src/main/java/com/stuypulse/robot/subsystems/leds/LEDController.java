0a0a776fbcf99ceaaa9a9cecbf441a94b1b7143c
aidenzhang21345 <aidenzhang211@gmail.com>
2026-04-25 11:11:39 -0400

    Remove led start and stop in periodic

--- src/main/java/com/stuypulse/robot/subsystems/leds/LEDController.java
+++ src/main/java/com/stuypulse/robot/subsystems/leds/LEDController.java
@@ -78,11 +78,10 @@ public class LEDController extends SubsystemBase {
     public void periodic() {
         if (!Settings.EnabledSubsystems.LED.get()) {
             LEDPattern.kOff.applyTo(buffer);
-            led.stop();
+            led.setData(buffer);
             return;
         }
 
-        led.start();
         led.setData(buffer);
     }
 } 
