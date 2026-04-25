c27bde2564090c0dba586ff3474832f339fa8d88
aidenzhang21345 <aidenzhang211@gmail.com>
2026-04-25 16:00:19 -0400

    CLEAN:Delete all other shooter commands except for ShooterCommands.java

--- src/main/java/com/stuypulse/robot/commands/shooter/ShooterSetManual.java
+++
@@ -1,9 +0,0 @@
-package com.stuypulse.robot.commands.shooter;
-
-import com.stuypulse.robot.subsystems.shooter.Shooter.ShooterState;
-
-public class ShooterSetManual extends ShooterSetState{
-    public ShooterSetManual() {
-        super(ShooterState.MANUAL_HUB);
-    }
-}
--- src/main/java/com/stuypulse/robot/commands/shooter/ShooterSetShoot.java
+++
@@ -1,9 +0,0 @@
-package com.stuypulse.robot.commands.shooter;
-
-import com.stuypulse.robot.subsystems.shooter.Shooter.ShooterState;
-
-public class ShooterSetShoot extends ShooterSetState{
-    public ShooterSetShoot(){
-        super(ShooterState.SHOOT);
-    }
-}
