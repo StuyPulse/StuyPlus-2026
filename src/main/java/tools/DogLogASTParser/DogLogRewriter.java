d65dac32202e8d682d1dd39a942dcfa02ef8f20a
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2026-04-25 20:40:10 -0400

    DOCS: Add doc for DogLogRewriter.java

--- src/main/java/tools/DogLogASTParser/DogLogRewriter.java
+++ src/main/java/tools/DogLogASTParser/DogLogRewriter.java
@@ -5,10 +5,29 @@ import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 
+/**
+ * <h2>DogLogRewriter | Actual functionality
+ * 
+ * <p>The actual class that performs the AST parsing and replacement of <code>SmartDashboard</code> calls with <code>DogLog</code> calls, invoked by the {@link Main} class.
+ * 
+ * <p>Extends {@link ModifierVisitor} to "visit" and modify the calls.
+ */
 public class DogLogRewriter extends ModifierVisitor<Void> {
 
     private boolean fileChanged = false;
 
+    /**
+     * <h4>Visit Method</h4>
+     * 
+     * <p>Overrides the {@link visit} method for method calls. 
+     * <p>It loops through every { @link MethodCallExpr} to check for a <code>SmartDashboard</code> call with the methods <code>putNumber</code>, <code>putBoolean</code>, or <code>putString</code>. If so, it replaces the scope with <code>DogLog</code> and changes the method name to <code>log</code>.
+     * 
+     * <p>Sets variable <code>fileChanged</code> to check if any changes were made to the file to determine whether to import <code>DogLog</code> at the end of the file.
+     * 
+     * @param methodCall the method call expression being visited
+     * @param arg additional argument (not used in this case)
+     * @return the modified method call expression
+     */
     @Override
     public MethodCallExpr visit(MethodCallExpr methodCall, Void arg) {
         super.visit(methodCall, arg);
@@ -43,6 +62,15 @@ public class DogLogRewriter extends ModifierVisitor<Void> {
         return methodCall;
     }
 
+    /**
+     * <h4>Applies the replacement</h4>
+     * 
+     * <p>Applies the <code>SmartDashboard</code> to <code>DogLog</code> replacement to the given { @link CompilationUnit}.
+     * 
+     * <p>If the <code>fileChanged</code> flag is true, it checks if the files already has an import for <code>DogLog</code>. If not, it adds the import statement. If the flag is false, it does nothing to avoid unnecessary changes and imports.
+     * 
+     * @param parsed the {@link CompilationUnit} to process
+     */
     public static void applyReplacement(CompilationUnit parsed) {
         DogLogRewriter doglogRewriter = new DogLogRewriter();
         parsed.accept(doglogRewriter, null);
