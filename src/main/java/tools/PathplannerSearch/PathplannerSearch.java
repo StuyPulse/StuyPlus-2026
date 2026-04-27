2a937b58945eceeff3e4e9e74777602d2f3fae31
Faizaan-J <125227430+Faizaan-J@users.noreply.github.com>
2026-04-27 18:20:14 -0400

    REFACTOR: Move PathplannerSearch tool into its own folder

---
+++ src/main/java/tools/PathplannerSearch/PathplannerSearch.java
@@ -0,0 +1,59 @@
+package tools.PathplannerSearch;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * <h3>Utility class that gives search features that Pathplanner lacks</h3>
+ * <p>This class is meant to help search for where paths and linked variables are used so you can clean up unused ones.
+ * <br>Use by first changing the SEARCH_TERM and type variables to indicate what you want to search for & the type of thing you are searching for. 
+ */
+public final class PathplannerSearch {
+    private enum SearchType {
+        LINKED_WAYPOINT,
+        PATH
+    }
+
+    public static void main(String[] args) throws IOException {
+        final String SEARCH_TERM = "YOUR_SEARCH_TERM_HERE".toLowerCase();
+        final SearchType type = SearchType.PATH;
+
+        switch (type) {
+            case LINKED_WAYPOINT -> {
+                final File[] files = new File("./src/main/deploy/pathplanner/paths/").listFiles();
+                if (files == null) break;
+
+                final List<String> matches = new ArrayList<>();
+                for (final File file : files) {
+                    final String content = Files.readString(file.toPath()).toLowerCase();
+                    if (content.isBlank()) continue;
+                    if (content.contains("\"linkedname\"") && content.contains("\"" + SEARCH_TERM + "\"")) {
+                        matches.add(file.getName());
+                    }
+                }
+                System.out.println("Paths that use the linked waypoint '" + SEARCH_TERM + "':\n" + (matches.isEmpty() ? "none" : String.join(", ", matches)));
+            }
+            case PATH -> {
+                final File[] files = new File("./src/main/deploy/pathplanner/autos/").listFiles();
+                if (files == null) break;
+
+                final List<String> matches = new ArrayList<>();
+                for (final File file : files) {
+                    final String content = Files.readString(file.toPath()).toLowerCase();
+                    if (!content.contains("\"sequential\"")) continue;
+                    if (content.contains("\"path\"") && content.contains("\"pathname\"") && content.contains("\"" + SEARCH_TERM + "\"")) {
+                        matches.add(file.getName());
+                    }
+                }
+                System.out.println("Autons that use the path '" + SEARCH_TERM + "':\n" + (matches.isEmpty() ? "none" : String.join(", ", matches)));
+            }
+            default -> {
+                System.out.println("Specify a valid search type by editing the file.");
+                System.exit(1);
+            }
+        }
+    }
+}
