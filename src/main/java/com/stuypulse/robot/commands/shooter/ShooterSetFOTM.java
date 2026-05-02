81631aa2a7195958d23f7b3524ec9283a99eb759
hc-45 <223769218+hc-45@users.noreply.github.com>
2026-05-02 17:11:53 -0400

    CLEAN: remove commands interfering with spotlessApply

--- src/main/java/com/stuypulse/robot/commands/shooter/ShooterSetFOTM.java
+++
@@ -1,9 +0,0 @@
-// package com.stuypulse.robot.commands.shooter;
-
-// import com.stuypulse.robot.subsystems.shooter.Shooter.ShooterState;
-
-// public class ShooterSetFOTM extends ShooterSetState {
-//     public ShooterSetFOTM(){
-//         super(ShooterState.FOTM);
-//     }
-// }
