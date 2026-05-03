8d85287c81caa8f6696f1488d8f2f669a4579a30
aidenzhang21345 <aidenzhang211@gmail.com>
2026-05-03 13:44:56 -0400

    REFACTOR: Replace Camera class with record, delete unused isActive field

--- src/main/java/com/stuypulse/robot/constants/Cameras.java
+++ src/main/java/com/stuypulse/robot/constants/Cameras.java
@@ -9,41 +9,29 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.util.Units;
 
-import com.stuypulse.stuylib.network.SmartBoolean;
-
-
 public class Cameras {
     public static final Camera[] LimelightCameras = new Camera[] {
         new Camera("limelight", 
             new Pose3d(Units.inchesToMeters(-12.109), Units.inchesToMeters(-7.129), Units.inchesToMeters(8.375), 
-            new Rotation3d(Units.degreesToRadians(180), Units.degreesToRadians(28), Units.degreesToRadians(180))), 
-            new SmartBoolean("Enabled", true))}; // TODO: get actual camera info
+            new Rotation3d(Units.degreesToRadians(180), Units.degreesToRadians(28), Units.degreesToRadians(180))))}; // TODO: get actual camera info
 
-    public static class Camera {
-        private String name;
-        private Pose3d location;
-        private SmartBoolean isActive;
+    public record Camera(String name, Pose3d location) {}
 
-        public Camera(String name, Pose3d location, SmartBoolean isActive) {
-            this.name = name;
-            this.location = location;
-            this.isActive = isActive;
-        }
+    // public static class Camera {
+    //     private String name;
+    //     private Pose3d location;
 
-        public String getName(){
-            return name;
-        }
+    //     public Camera(String name, Pose3d location) {
+    //         this.name = name;
+    //         this.location = location;
+    //     }
 
-        public Pose3d getLocation() {
-            return location;
-        }
+    //     public String getName(){
+    //         return name;
+    //     }
 
-        public boolean isEnabled() {
-            return isActive.get();
-        }
-
-        public void setEnabled(boolean enabled) {
-            this.isActive.set(enabled);
-        }
-    }
+    //     public Pose3d getLocation() {
+    //         return location;
+    //     }
+    // }
 }
