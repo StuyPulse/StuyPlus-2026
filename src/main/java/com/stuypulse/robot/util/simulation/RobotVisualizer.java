1b0651b1d714916da31b0e1290e73f3769366c13
hc-45 <223769218+hc-45@users.noreply.github.com>
2026-05-17 17:38:46 -0400

    DOCS + REFACTOR: document RobotVisualizer, rename and abstract a few things

--- src/main/java/com/stuypulse/robot/util/simulation/RobotVisualizer.java
+++ src/main/java/com/stuypulse/robot/util/simulation/RobotVisualizer.java
@@ -18,11 +18,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
+/**
+ * <h2>Combined robot visualizer for all of the subsystems in one canvas.</h2>
+ * 
+ * <p>
+ * This class employs the singleton pattern to ensure that only one visualizer
+ * exists,
+ * since only one robot instance should exist.
+ * </p>
+ */
 public class RobotVisualizer {
-
     public static RobotVisualizer instance;
 
     static {
@@ -33,53 +39,100 @@ public class RobotVisualizer {
         return instance;
     }
 
+    private static final double CANVAS_WIDTH = 67, CANVAS_HEIGHT = 67;
+
+    /**
+     * <h4>Number of spokes for wheels</h4>
+     * 
+     * @see #createSpokes()
+     */
+    private static final int NUM_SPOKES = 5;
+    /**
+     * <h4>Color of spokes for wheels</h4>
+     * 
+     * @see #createSpokes()
+     */
+    private static final Color8Bit SPOKE_COLOR = new Color8Bit(Color.kWhite);
+
+    /**
+     * <h4>Sendable main canvas object for every drawn mechanism</h4>
+     * 
+     * @see #createSpokes()
+     */
     private final Mechanism2d canvas;
 
-    private final double width, height;
-
-    private final int numSpokes;
-
-    private final Color8Bit spokeColor;
-
-    // silhouette
+    /**
+     * <p>Root of the {@link #bumper red} bumpers to show the silhouette of the robot<p>
+     */
     private final MechanismRoot2d bumperRoot;
-
+    /**
+     * <p>Drawn representation of red bumpers to show the silhouette of the robot</p>
+     * 
+     * @see #bumperRoot
+     */
     private final MechanismLigament2d bumper;
 
-    // //feeder:
+    /**
+     * <h4>Root for the drawn representation of the feeder</h4>
+     * 
+     * <p>The actual feeder is a belt, but spokes are easier. Additionally, the {@link #feederSpokes spokes} are placed where the handoff should be...</p>
+     */
     private final MechanismRoot2d feederRoot;
 
+    /**
+     * <p>Spokes of the feeder</p>
+     * 
+     * @see #feederRoot
+     */
     private final MechanismLigament2d[] feederSpokes;
 
-    // intake:
+    /**
+     * <p>Root for the drawn representation of the intake</p>
+     * 
+     * @see #intakePivot
+     * @see #intakeSpokes
+     */
     private final MechanismRoot2d intakeRoot;
-
+    /**
+     * <p>Pivoting arm of the intake.
+     * Parent of the {@link #intakeSpokes intake rollers}.</p>
+     */
     private final MechanismLigament2d intakePivot;
+    /**
+     * <h4>Intake Rollers</h4>
+     * <p>This is a flattened array of every set of rollers for easy iteration.</p>
+     * 
+     * @see #RobotVisualizer()
+     */
+    private final MechanismLigament2d[] intakeSpokes;
 
-    private final List<MechanismLigament2d> intakeSpokes;
-
-    // shooter:
+    /**
+     * <p>Root for the drawn representation of the shooter</p>
+     */
     private final MechanismRoot2d shooterRoot;
-
+    /**
+     * <p>Shooter rollers</p>
+     * 
+     * @see #shooterRoot
+     */
     private final MechanismLigament2d[] shooterSpokes;
-
+    /**
+     * <h4>Constructs the visualizer instance</h4>
+     * <p>Uses specific internal logic, viewing the source code is recommended to better understand how everything is setup.</p>
+     */
     private RobotVisualizer() {
-        width = 67;
-        height = 67;
-        numSpokes = 5;
-        spokeColor = new Color8Bit(Color.kWhite);
-        canvas = new Mechanism2d(width, height);
+        canvas = new Mechanism2d(CANVAS_WIDTH, CANVAS_HEIGHT);
         // Silhouette
         bumperRoot = canvas.getRoot("Bumper Root", 10, 5);
         bumper = new MechanismLigament2d("Bumper", 50, 0, 90, new Color8Bit(Color.kRed));
         bumperRoot.append(bumper);
-        // // Feeder
+        // Feeder
         feederRoot = canvas.getRoot("Feeder Root", 45, 15);
-        feederSpokes = createSpokes(numSpokes, feederRoot, "Feeder Spoke", 6.7, 2, spokeColor);
+        feederSpokes = createSpokes(feederRoot, "Feeder Spoke", 6.7, 2);
         // Shooter
         shooterRoot = canvas.getRoot("Shooter Root", 55, 45);
-        shooterSpokes = createSpokes(numSpokes, shooterRoot, "Shooter Spoke", 6.7, 2, spokeColor);
-        // Intake :D
+        shooterSpokes = createSpokes(shooterRoot, "Shooter Spoke", 6.7, 2);
+        // Intake
         intakeRoot = canvas.getRoot("Intake Root", 15, 10);
         intakePivot = new MechanismLigament2d(
                 "Intake Arm",
@@ -97,64 +150,123 @@ public class RobotVisualizer {
         intakeBottomRollers.append(intakeMiddleRollers);
         intakeMiddleRollers.append(intakeTopRollers);
         {
+            /* 
+             * Draw the other lines in the intake structure.
+             * We do this Within a separate scope using {} to ensure the _a and _b variables are temporary.
+             */
             var _a = new MechanismLigament2d("_a", 8, 50, 4, new Color8Bit(Color.kGray));
             _a.append(intakeBottomRollers);
             var _b = new MechanismLigament2d("_b", 13.75, 95, 4, new Color8Bit(Color.kGray));
             intakeBottomRollers.append(_b);
             intakePivot.append(_a);
         }
+        // Flatten all the sets of intake rollers into one big array
         intakeSpokes = Arrays.stream(
                 new MechanismLigament2d[][] {
-                        createSpokes(numSpokes, intakeTopRollers, "Intake Top Spoke", .67, 2, spokeColor),
-                        createSpokes(
-                                numSpokes, intakeMiddleRollers, "Intake Middle Spoke", .67, 2, spokeColor),
-                        createSpokes(
-                                numSpokes, intakeBottomRollers, "Intake Bottom Spoke", .67, 2, spokeColor)
+                        createSpokes(intakeTopRollers, "Intake Top Spoke", .67, 2),
+                        createSpokes(intakeMiddleRollers, "Intake Middle Spoke", .67, 2),
+                        createSpokes(intakeBottomRollers, "Intake Bottom Spoke", .67, 2)
                 })
                 .flatMap(Arrays::stream)
-                .collect(Collectors.toList());
+                .toArray(MechanismLigament2d[]::new);
+        // Publish the canvas to Smartdashboard
         SmartDashboard.putData("Visualizers/Robot", canvas);
     }
 
+    /**
+     * 
+     * <h4>Helper to create wheels</h4>
+     * 
+     * <p>Creates a simulacrum of a wheel or side view of a roller
+     * by making a bunch of equally-spaced, outwards-radiating lines that like are spokes of a wheel</p>
+     * @param target the object to append the spokes to
+     * @param name name of each spoke, with name mangling applied internally
+     * @param length length of the spokes
+     * @param width width of the spokes
+     * @param color color of the spokes
+     * @return An array of the generated spokes
+     */
     private MechanismLigament2d[] createSpokes(
-            int num,
             MechanismObject2d target,
             String name,
             double length,
             double width,
             Color8Bit color) {
-        MechanismLigament2d[] spokes = new MechanismLigament2d[num];
-        double spacing = 360 / num;
-        for (int i = 0; i < num; i++)
+        MechanismLigament2d[] spokes = new MechanismLigament2d[NUM_SPOKES];
+        double spacing = 360 / NUM_SPOKES;
+        for (int i = 0; i < NUM_SPOKES; i++)
             spokes[i] = target.append(
                     new MechanismLigament2d(name.trim() + " " + i, length, spacing * i, width, color));
         return spokes;
     }
 
+    /**
+     * 
+     * <h4>Helper to create wheels</h4>
+     * 
+     * <p>Creates a simulacrum of a wheel or side view of a roller
+     * by making a bunch of equally-spaced, outwards-radiating lines that like are spokes of a wheel.
+     * Defaults to the statically specified color {@link #SPOKE_COLOR}</p>
+     * @param target the object to append the spokes to
+     * @param name name of each spoke, with name mangling applied internally
+     * @param length length of the spokes
+     * @param width width of the spokes
+     * @return An array of the generated spokes
+     */
+    private MechanismLigament2d[] createSpokes(
+            MechanismObject2d target,
+            String name,
+            double length,
+            double width) {
+        return this.createSpokes(target, name, length, width, SPOKE_COLOR);
+    }
+
+    /**
+     * <h4>Update method for the feeder</h4>
+     * 
+     * <p>To be called by subsystems using motor measurements</p>
+     * @param angularVelocity measured angular velocity of the feeder rollers
+     */
     public void updateFeeder(AngularVelocity angularVelocity) {
         double rot = angularVelocity.in(RPM) * 6 * Settings.DT.in(Seconds);
         for (MechanismLigament2d spoke : feederSpokes)
             spoke.setAngle(spoke.getAngle() + rot);
     }
 
+    /**
+     * <h4>Update method for the shooter</h4>
+     * 
+     * <p>To be called by subsystems using motor measurements</p>
+     * @param angularVelocity measured angular velocity of the shooter rollers
+     */
     public void updateShooter(AngularVelocity angularVelocity) {
         double rot = angularVelocity.in(RPM) * 6 * Settings.DT.in(Seconds);
         for (MechanismLigament2d spoke : shooterSpokes)
             spoke.setAngle(spoke.getAngle() + rot);
     }
 
+    /**
+     * <h4>Update method for the intake</h4>
+     * 
+     * <p>To be called by subsystems using motor measurements</p>
+     * @param pivotAngle measured position of the intake pivot
+     * @param angularVelocity measured angular velocity of the intake rollers
+     */
     public void updateIntake(Angle pivotAngle, AngularVelocity angularVelocity) {
         intakePivot.setAngle(pivotAngle.in(Degrees) + 102); // counteract the weird zeroing
         double rot = angularVelocity.in(RPM) * Settings.DT.in(Seconds);
         for (MechanismLigament2d spoke : intakeSpokes)
             spoke.setAngle(spoke.getAngle() + rot);
-        // SmartDashboard.putData("Visualizers/Robot", canvas);
     }
 
+    @Deprecated
     public Angle getIntakeAngle() {
         return Degrees.of(intakePivot.getAngle());
     }
 
+    /**
+     * <p>Publish the canvas to {@link SmartDashboard}</p>
+     */
     public void update() {
         SmartDashboard.putData("Visualizers/Robot", canvas);
     }
