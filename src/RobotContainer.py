2fb2279edc3dd4ef2fc6a000ebc3ea8b7255cc12
Faizaan J <125227430+Faizaan-J@users.noreply.github.com>
2026-05-26 17:25:04 -0400

    FEAT: Write Robot.py

---
+++ src/RobotContainer.py
@@ -0,0 +1,4 @@
+class RobotContainer:
+    def __init__(self):
+        pass
+    
--- src/robot.py
+++ src/robot.py
@@ -1,1 +1,53 @@
 # TODO: insert robot code here
+from wpilib import TimedRobot
+from wpilib import DriverStation
+from wpilib import SmartDashboard
+from wpilib import DataLogManager
+
+from phoenix6 import signal_logger
+
+from commands2 import Command
+from commands2 import commandscheduler
+
+from RobotContainer import RobotContainer
+from typing import ClassVar
+
+class Robot(TimedRobot):
+    __RobotContainer : RobotContainer
+    __auto : Command
+    __alliance : ClassVar[DriverStation.Alliance]
+
+    @staticmethod
+    def isBlue() -> bool:
+        # Return whether the current alliance is blue
+        return DriverStation.getAlliance() == DriverStation.Alliance.Blue
+
+
+    def robotInit(self):
+        self.__RobotContainer = RobotContainer()
+        # Initialize the stored alliance value
+        if (DriverStation.isDSAttached()):
+            self.__alliance = DriverStation.getAlliance()
+        else:
+            self.__alliance = DriverStation.Alliance.kBlue
+
+        DataLogManager.start()
+        DataLogManager.logNetworkTables(True)
+        print("]LOGGING DIRECTORY]: " + DataLogManager.getLogDir())
+        signal_logger.SignalLogger.start()
+
+    def driverStationConnected(self):
+        self.__alliance = DriverStation.getAlliance()
+
+    def robotPeriodic(self):
+        commandscheduler.CommandScheduler.getInstance().run()
+        SmartDashboard.putNumber("Bot/Alliance", self.__alliance.name)
+        SmartDashboard.putNumber("Match Time", DriverStation.getMatchTime())
+        SmartDashboard.putData("Command Scheduler", commandscheduler.CommandScheduler.getInstance())
+
+    def teleopInit(self):
+        autonWon = DriverStation.getGameSpecificMessage() == (self.__alliance.name[0].upper())
+        SmartDashboard.putBoolean("Auton Won", autonWon)
+    
+    def testInit(self):
+        commandscheduler.CommandScheduler.getInstance().cancelAll()
