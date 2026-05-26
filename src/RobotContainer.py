f501577a1fc39a06787a9ea9bca9cc95074f80ff
Faizaan J <125227430+Faizaan-J@users.noreply.github.com>
2026-05-26 17:31:12 -0400

    FEAT: Scaffold RobotContainer.py

--- src/RobotContainer.py
+++ src/RobotContainer.py
@@ -1,4 +1,29 @@
+import wpilib
+import commands2
+
+from typing import ClassVar
+
 class RobotContainer:
+    # Gamepad
+    driver : commands2.button.CommandXboxController 
+
+    # Subsystems
+    # TODO: insert subsystems here once implemented
+
+    # Autons
+    __auton_chooser : ClassVar[wpilib.SendableChooser[commands2.Command]]
+
     def __init__(self):
         pass
     
+    def configureDefaultCommands(self):
+        pass
+
+    def configureButtonBindings(self):
+        pass
+    
+    def configureAutons(self):
+        pass
+
+    def getAutonomousCommand(self) -> commands2.Command:
+        return self.__auton_chooser.getSelected()
